6f2d70b982219dfabfde2b78690ed32ae82a5d0b
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-02-12 22:40:25 +0000

    Add a router

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import routes from './router/routes.js';
 
 const port = process.env.PORT || 5000;
 
 //Init the app
 const app = express();
 
+//Main router
+app.use("/", routes)
 
 //Listening on the port
 app.listen(port, () => {
