2f24187d73debe28a47499ce1ae0bca41a518764
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-02-13 00:31:02 +0000

    Create a function to retrieve the data of one item using the id, table and fields

--- db/queries.js
+++ db/queries.js
@@ -37,3 +37,34 @@ async function all(table = "games", fields = "*"){
         throw new Error("Database query failed :" + error.message);
     }
 }
+
+/**
+ * 
+ * @param {INTEGER} id The id of the selected game
+ * @param {*} table The table in which to execute the query
+ * @param {*} fields The fields to use to retrieve the data
+ * @returns Retrieve the data of one game
+ */
+async function find(id, table = "games", fields = "*"){
+    if(!allowedTables.includes(table)){
+        throw new Error("Table not allowed");
+    }
+
+    if(fields !== "*"){
+        fields.split(",").forEach(field => {
+            const trimmedField = field.trim();
+            if(!allowedFields[table].includes(trimmedField)){
+                throw new Error(`Field '${trimmedField} not allowed in '${table}`)
+            }
+        });
+    }
+
+    const query = `SELECT ${fields} from ${table} WHERE id=$1`;
+
+    try {
+        const { rows } = await pool.query(query, [id]);
+        return rows;
+    } catch (error) {
+        throw new Error("Database query failed : " + error.message );
+    }
+}
