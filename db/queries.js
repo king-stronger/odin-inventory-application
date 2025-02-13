7a4761e9e4f1df0e2557fa0c79449fa84b59f0e4
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-02-13 00:17:00 +0000

    Create a function to retrieve all data using the table and fields

--- db/queries.js
+++ db/queries.js
@@ -6,3 +6,34 @@ const allowedFields = {
     genres: ["name"],
     developers: ["name"],
 }
+
+
+/**
+ * 
+ * @param {String} table The table in which to execute the query
+ * @param {String} fields The fields to use to retrieve the data
+ * @returns Retrieve the data using the table and fields
+ */
+async function all(table = "games", fields = "*"){
+    if(!allowedTables.includes(table)){
+        throw new Error("Table not allowed");
+    }
+
+    if(fields !== "*"){
+        fields.split(",").forEach(field => {
+            const trimmedField = field.trim();
+            if(!allowedFields[table].includes(trimmedField)){
+                throw new Error(`Field '${trimmedField}' not allowed in ${table}`);
+            }
+        });
+    }
+
+    const query = `SELECT ${fields} from ${table}`;
+
+    try {
+        const { rows } = await pool.query(query);
+        return rows;
+    } catch (error) {
+        throw new Error("Database query failed :" + error.message);
+    }
+}
