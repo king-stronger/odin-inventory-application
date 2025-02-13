1a9cd537567abcab1b0760f75dfd08f766751903
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-02-13 00:10:09 +0000

    Add comments

--- router/routes.js
+++ router/routes.js
@@ -11,18 +11,25 @@ import {
 
 const router = Router();
 
+// Get all games
 router.get("/", getAllGames);
 
+// Send a form to create a game
 router.get("/create", createGame);
 
+// Find a game
 router.get("/:id", findGame);
 
+// Edit a game
 router.get("/edit/:id", editGame);
 
+// Store the game into the database
 router.post("/create", storeGame);
 
+// Update the game in the database
 router.put("/edit/:id", updateGame);
 
+// Delete the game in the databse
 router.delete("/:id", deleteGame);
 
 export default router;
