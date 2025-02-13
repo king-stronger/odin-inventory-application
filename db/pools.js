4efe02f8112f8f7fb2504b3382ddd794b620f85d
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-02-13 00:11:08 +0000

    Create a pool to interact with the database

---
+++ db/pools.js
@@ -0,0 +1,13 @@
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
+});
+
+export default pool;
