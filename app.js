29807c725343e67060c9c41d480e9a39e128ba47
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-02-13 16:23:03 +0000

    Add middlewares to handle form and json data submission

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ import routes from './router/routes.js';
 
 const port = process.env.PORT || 5000;
 
-//Init the app
+// Init the app
 const app = express();
 
-//Game routes
+// Express middlewares to handle form or json data submission
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Game routes
 app.use("/games", routes)
 
-//Listening on the port
+// Listening on the port
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
