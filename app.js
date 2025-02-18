35d952aa8cdeef53c86e87cecc29a0c42c0da38d
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-02-18 19:00:24 +0000

    Set the ejs template engine

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+import path from 'path';
 import express from 'express';
+import { fileURLToPath } from 'url';
 import routes from './router/routes.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 const port = process.env.PORT || 5000;
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Init the app
 const app = express();
@@ -10,8 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Set ejs template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(dirname, "views"));
+
 // Game routes
-app.use("/games", routes)
+app.use("/games", routes);
 
 // Listening on the port
 app.listen(port, () => {
